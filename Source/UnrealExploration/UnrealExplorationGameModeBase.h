e7616ff9fc9c06e7d9720cb7920438f3cfe500e4
Shane Lillie <ignignokterr@gmail.com>
2021-10-13 07:48:04 -0700

    Initial import

---
+++ Source/UnrealExploration/UnrealExplorationGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealExplorationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALEXPLORATION_API AUnrealExplorationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
